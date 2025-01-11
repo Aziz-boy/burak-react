0afd81b986ca9ece16e954a7d61bf36e68e376ef
Aziz-boy <shavgoniaziz@gmail.com>
2025-01-11 16:26:37 +0500

    feat: Create Routing System

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter as Router } from "react-router-dom";
 import theme from './app/MaterialTheme';
 import './css/index.css';
 
+const container = document.getElementById('root'); // Get the root container element
+if (!container) throw new Error("Root container is missing in index.html");
+
+const root = ReactDOM.createRoot(container); // Create a root
 
 
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}> 
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
