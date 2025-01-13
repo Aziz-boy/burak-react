1082aab9097156e529f6851b002ddb44c0ae0d62
Aziz-boy <shavgoniaziz@gmail.com>
2025-01-13 18:25:51 +0500

    fix: fixed typo bug

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -4,44 +4,48 @@ export function HomeNavbar() {
     const authMember = null;
     return(
     <div className="home-navbar">
-        <Container >
+        <Container className='navbar-container'>
             <Stack className='menu'>
                 <Box>
                     <NavLink to="/">
-                        <img alt='Logo' className='brand-logo' src="/icons/burak.svg"/>
+                    <img 
+                        className='brand-logo'
+                        alt='Logo' 
+                        src="/icons/burak.svg"
+                    />
                     </NavLink>
                 </Box>
                 <Stack className='links'>
                 <Box className={"hover-line"}>
-                    <NavLink to="/" activeClassName={"undeline"}>Home</NavLink>
+                    <NavLink to="/" activeClassName={"underline"}>Home</NavLink>
                 </Box> 
                 <Box className={"hover-line"}>
-                    <NavLink to="/product" activeClassName={"undeline"}>Products</NavLink>
+                    <NavLink to="/product" activeClassName={"underline"}>Products</NavLink>
                 </Box> 
                 { authMember ?  (
                 <Box className={"hover-line"}>
-                    <NavLink to="/orders" activeClassName={"undeline"}>Orders</NavLink>
+                    <NavLink to="/orders" activeClassName={"underline"}>Orders</NavLink>
                 </Box> 
                 ) : null }
                 { authMember ?  (
                 <Box className={"hover-line"}>
-                    <NavLink to="/member-page" activeClassName={"undeline"}>My Page</NavLink>
+                    <NavLink to="/member-page" activeClassName={"underline"}>My Page</NavLink>
                 </Box> 
                 ) : null }
                 <Box className={"hover-line"}>
-                    <NavLink to="/help" activeClassName={"undeline"}>Help</NavLink>
+                    <NavLink to="/help" activeClassName={"underline"}>Help</NavLink>
                 </Box> 
                 { /* BASKET */ }
                 {!authMember ? (
                     <Box>
-                        <Button variant='contained' className='login-button'>Login</Button>
+                        <Button variant='contained' className='login-button' >Login</Button>
                     </Box>
                 ) : (
                     <img 
-                    alt='Profile-picture' 
-                    className='user-avatar'
-                    src="/icons/default-user.svg"  
-                    aria-haspopup="true"
+                        alt='Profile-picture' 
+                        className='user-avatar'
+                        src={"/icons/default-user.svg"}
+                        aria-haspopup={"true"}
                     />
                 )}
                 </Stack>
