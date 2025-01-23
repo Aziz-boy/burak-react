afacc7d28a3cbeee3fcbf41769b967e6fe2ccddc
Aziz-boy <shavgoniaziz@gmail.com>
2025-01-24 01:01:35 +0500

    feat: hover logic in products fixed

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import Badge from "@mui/material/Badge";
 import Pagination from "@mui/material/Pagination";
 import PaginationItem from "@mui/material/PaginationItem";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import ShoppingCartSharpIcon from "@mui/icons-material/ShoppingCartSharp";
-import AspectRatio from "@mui/joy/AspectRatio";
-import Card from "@mui/joy/Card";
-import CardOverflow from "@mui/joy/CardOverflow";
-import { CssVarsProvider } from "@mui/joy";
 
 const products = [
-  { productName: "Korean Spicy Soup", imagePath: "/img/cutlet.webp" },
-  { productName: "Vegetarian Soup", imagePath: "/img/kebab-fresh.webp" },
-  { productName: "Salmon Pasta", imagePath: "/img/kebab.webp" },
-  { productName: "Kebuli Rice", imagePath: "/img/lavash.webp" },
+  { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
+  { productName: "Kebab", imagePath: "/img/kebab.webp" },
+  { productName: "Lavash", imagePath: "/img/lavash.webp" },
   { productName: "Lavash", imagePath: "/img/lavash.webp" },
   { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
   { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
@@ -28,157 +22,103 @@ const products = [
 
 export default function Products() {
   return (
-    <div className="products">
-      <Container>
-        <Stack flexDirection={"column"} alignItems={"center"}>
-          <Stack className="avatar-big-box">
-            <Box className="category-title">Burak Restaurant</Box>
-            <Stack className="input-box">
-              <Box className="search-input">
+    <div className={"products"}>
+      <Container sx={{ padding: "0E" }}>
+        <Stack
+          flexDirection={"column"}
+          sx={{ width: "1300px" }}
+          alignItems={"center"}
+        >
+          <Stack className={"avatar-big-box"}>
+            <Stack className="top-content">
+              <Box className="text">Burak Restaurant</Box>
+              <Box className="search-container">
                 <input
-                  className="input-field"
-                  placeholder="Type here..."
-                  style={{
-                    padding: "10px 20px", // Add padding to input
-                    fontSize: "16px",
-                  }}
+                  type="text"
+                  className="search-input"
+                  placeholder="Type here"
                 />
-              </Box>
-              <Box className="search-button">
-                <Button variant="contained" className="search-text">
-                  SEARCH <SearchIcon className="search-icon" />
-                </Button>
+                <button className="search-button">
+                  SEARCH <SearchIcon />
+                </button>
               </Box>
             </Stack>
           </Stack>
 
-          <Stack className="dishes-filter-section">
-            <Button variant="contained" className="filter-button">
-              NEW
-            </Button>
-            <Button
-              variant="contained"
-              className="filter-button"
-              color="secondary"
-            >
-              PRICE
-            </Button>
-            <Button
-              variant="contained"
-              className="filter-button"
-              color="secondary"
-            >
-              VIEWS
-            </Button>
+          <Stack className={"dishes-filter-section"}>
+            <Stack className="dishes-filter-box">
+              <Button variant="contained" color="primary" className="order">
+                New
+              </Button>
+              <Button variant="contained" color="secondary" className="order">
+                Price
+              </Button>
+              <Button variant="contained" color="secondary" className="order">
+                Views
+              </Button>
+            </Stack>
           </Stack>
 
           <Stack className={"list-category-section"}>
-            <Stack className={"product-category"}>
-              <Button variant="contained" className="filter-button">
-                DISH
-              </Button>
-              <Button
-                variant="contained"
-                className="filter-button"
-                color="secondary"
-              >
-                SALAD
-              </Button>
-              <Button
-                variant="contained"
-                className="filter-button"
-                color="secondary"
-              >
-                DRINK
-              </Button>
-              <Button
-                variant="contained"
-                className="filter-button"
-                color="secondary"
-              >
-                DESERT
-              </Button>
-              <Button
-                variant="contained"
-                className="filter-button"
-                color="secondary"
-              >
-                OTHER
-              </Button>
+            <Stack className="product-category">
+              <div className="category-main">
+                <Button variant="contained" color="secondary">
+                  Other
+                </Button>
+                <Button variant="contained" color="secondary">
+                  Desert
+                </Button>
+                <Button variant="contained" color="secondary">
+                  Drink
+                </Button>
+                <Button variant="contained" color="secondary">
+                  Salad
+                </Button>
+                <Button variant="contained" color="primary">
+                  Dish
+                </Button>
+              </div>
             </Stack>
 
-            <Stack className={"product-wrapper"}>
-              <CssVarsProvider>
-                {products.length !== 0 ? (
-                  products.map((ele, index) => {
-                    return (
-                      <Card key={index} variant="soft" className="card">
-                        <CardOverflow>
-                          <div className="product-sale">LARGE size</div>
-                          <AspectRatio ratio="1">
-                            <img src={ele.imagePath} alt="" />
-                          </AspectRatio>
-
-                          {/* Hover Effect Container */}
-                          <div className="hover-icons">
-                            <button
-                              className="icon-button"
-                              style={{
-                                width: "64px", // Set equal width
-                                height: "46px", // Set equal height
-                                display: "flex", // Center content
-                                alignItems: "center",
-                                justifyContent: "center",
-                                backgroundColor: "transparent", // Optional: Add background color
-                                borderRadius: "10px",
-                                border: "1px solid #E3C08D",
-                                background: "rgba(187, 47, 200, 0.78)",
-                              }}
-                            >
-                              <ShoppingCartSharpIcon className="shopping-cart-icon" />
-                            </button>
-                            <button
-                              className="icon-button"
-                              style={{
-                                width: "40px", // Set equal width
-                                height: "40px", // Set equal height
-                                borderRadius: "50%", // Make it round
-                                display: "flex", // Center content
-                                alignItems: "center",
-                                justifyContent: "center",
-                                backgroundColor: "transparent", // Optional: Add background color
-                                border: "1px solid rgba(0, 0, 0, 0.40)",
-                              }}
-                            >
-                                <RemoveRedEyeIcon className="removed-eye-icon" />
-                              
-                            </button>
-                          </div>
-                        </CardOverflow>
-
-                        <CardOverflow variant="soft" className="product-detail">
-                          <Stack className="info">
-                            <Stack flexDirection={"column"}>
-                              <Typography className={"title"}>
-                                {ele.productName}
-                              </Typography>
-                              <Typography className={"price"}>
-                                <MonetizationOnIcon className="monetization-icon" />{" "}
-                                15
-                              </Typography>
-                              {/* Savatchani hover holida qo'shish kerak  */}
-                            </Stack>
-                          </Stack>
-                        </CardOverflow>
-                      </Card>
-                    );
-                  })
-                ) : (
-                  <Box className="no-data">
-                    New products are not available !
-                  </Box>
-                )}
-              </CssVarsProvider>
+            <Stack className="product-wrapper">
+              {products.length !== 0 ? (
+                products.map((product, index) => {
+                  return (
+                    <Stack key={index} className="product-card">
+                      <Stack
+                        className="product-img"
+                        sx={{ backgroundImage: `url(${product.imagePath})` }}
+                      >
+                        <div className="product-sale">Normal size</div>
+                        <Button className="shop-btn">
+                          <img
+                            src="/icons/shopping-cart.svg"
+                            style={{ display: "flex" }}
+                          />
+                        </Button>
+                        <Button className="view-btn" sx={{ right: "36px" }}>
+                          <Badge badgeContent={20} color="secondary">
+                            <RemoveRedEyeIcon
+                              sx={{ color: true ? "gray" : "white" }}
+                            />
+                          </Badge>
+                        </Button>
+                      </Stack>
+                      <Box className="product-desc-box">
+                        <span className="product-title">
+                          {product.productName}
+                        </span>
+                        <div className="product-desc">
+                          <MonetizationOnIcon />
+                          {12}
+                        </div>
+                      </Box>
+                    </Stack>
+                  );
+                })
+              ) : (
+                <Box className="no-data">Products are not available</Box>
+              )}
             </Stack>
           </Stack>
 
@@ -201,42 +141,36 @@ export default function Products() {
         </Stack>
       </Container>
 
-      <div className="brands-logo">
-        <Container>
-          <Typography className="brands-title">Our Family Brands</Typography>
-
-          <Stack className="brands-cards">
-            <Box className="brands-card">
-              <img src="./img/brand-red.svg" alt="" />
+      <div className={"brand-logo"}>
+        <Container className="family-brands">
+          <Box className="category-title">Our Family Brands</Box>
+          <Stack className="brand-list">
+            <Box className="review-box">
+              <img src="/img/gurme.webp" />
             </Box>
-            <Box className="brands-card">
-              <img src="./img/brand-blue.svg" alt="" />
+            <Box className="review-box">
+              <img src="/img/seafood.webp" />
             </Box>
-            <Box className="brands-card">
-              <img src="./img/brand-green.svg" alt="" />
+            <Box className="review-box">
+              <img src="/img/sweets.webp" />
             </Box>
-            <Box className="brands-card">
-              <img src="./img/brand-orange.svg" alt="" />
+            <Box className="review-box">
+              <img src="/img/doner.webp" />
             </Box>
           </Stack>
         </Container>
       </div>
 
-      <div className="address">
+      <div className={"address"}>
         <Container>
-          <Stack className="address-area">
-            <Typography className="address-title">Our address</Typography>
-
+          <Stack className="address-are" sx={{ width: "1300px" }}>
+            <Box className="title">Our address</Box>
             <iframe
-              style={{
-                marginTop: "60px",
-                width: "100%",
-                height: "500px",
-                border: "none",
-              }}
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3127997.171246206!2d62.2855628670045!3d40.037817679791395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38b5d30e40e38ce3%3A0xe50860cafe8775b7!2sRestaurant%20CZN%20BURAK!5e0!3m2!1sen!2s!4v1737267938505!5m2!1sen!2s"
-              title="CZN Burak Restaurant Google Map"
-              allowFullScreen
+              style={{ marginTop: "60px" }}
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d47974.52215811875!2d69.26707855177827!3d41.27823197720393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2skr!4v1737127589590!5m2!1sen!2skr"
+              width={"1300"}
+              height={"600"}
+              referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </Stack>
         </Container>
