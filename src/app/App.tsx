1b4bda07d2be424ec823a50abe0ed58c5e4daef5
Aziz-boy <shavgoniaziz@gmail.com>
2025-02-06 12:50:03 +0500

    feat: create Test class component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,6 +11,7 @@ import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import Test from "./screens/Test";
 
 function App() {
   const location = useLocation(); //react router domning hooki object return qiladi
@@ -33,7 +34,8 @@ function App() {
           <HelpPage />
         </Route>
         <Route path="/">
-          <HomePage />
+          <Test />
+          {/* <HomePage /> */}
         </Route>
       </Switch>
       <Footer />
