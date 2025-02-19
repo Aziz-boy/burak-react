c1f9b47692b32134fb201d6932f75a5dc8ef7e0c
Aziz-boy <shavgoniaziz@gmail.com>
2025-02-19 10:25:36 +0500

    feat: develop productsPage slice and selector

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,14 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import HomePageReducer from './screens/homePage/slice';
 import reduxLogger from "redux-logger";
+import ProductPageReducer from "./screens/productsPage/slice";
 
 export const store = configureStore({
   // @ts-ignore
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(reduxLogger),
   reducer: {
-    homePage: HomePageReducer
+    homePage: HomePageReducer,
+    productsPage: ProductPageReducer
   },
 });
 
--- src/lib/types/screen.ts
+++ src/lib/types/screen.ts
@@ -4,21 +4,22 @@ import { Member } from "./member";
 import { Product } from "./product";
 
 export interface AppRootState {
-    homePage: HomePageState;
-    // productsPage: ProductsPageState;
-
+  homePage: HomePageState;
+  productsPage: ProductsPageState;
 }
 
 /** HOMEPAGE **/
 export interface HomePageState {
-    popularDishes: Product[];
-    newDishes: Product[];
-    topUsers: Member[];
+  popularDishes: Product[];
+  newDishes: Product[];
+  topUsers: Member[];
 }
 
-
 /** PRODUCTS PAGE **/
-
-
+export interface ProductsPageState {
+  restaurant: Member | null;
+  chosenProduct: Product | null;
+  products: Product[];
+}
 
 /** ORDERS PAGE **/
