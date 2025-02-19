33bfbe44e84103255c27e38f9ca80996991a6730
Aziz-boy <shavgoniaziz@gmail.com>
2025-02-19 16:48:38 +0500

    fix: modify Chosen product component logic

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -34,7 +34,7 @@ export default function Products() {
 
   const [productSearch, setProductSearch] = useState<ProductInquiry>({
     page: 1,
-    limit: 8,
+    limit: 6,
     order: "createdAt",
     productCollection: ProductCollection.DISH,
     search: "",
